7f6b166fe6f3360047e39c55f60c0b6dc689a5ee
kamalnath7171 <kamalnath7171@gmail.com>
2023-08-05 16:08:14 -0400

    initial-commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask,jsonify,request, render_template
+
+app = Flask(__name__,template_folder='Templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/piechart')
+def piechart():
+    return render_template('piechart.html')
+
+if __name__ =="__main__":
+    app.run(host="0.0.0.0",port=3000)
